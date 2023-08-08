a5fb1d0b18003f859f1a2fae558d1a3649edfd77
Huy Ong <ogh.huy@gmail.com>
2023-08-08 07:26:19 -0700

    First Commit

---
+++ SkillSwapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkillSwapper
+//
+//  Created by Huy Ong on 8/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkillSwapper/SkillSwapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillSwapperApp.swift
+//  SkillSwapper
+//
+//  Created by Huy Ong on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SkillSwapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
